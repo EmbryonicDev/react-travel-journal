f53a3a999a68397260c3b3e6d83d4191bb873482
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-10 14:31:19 +0200

    Add 2 x objects to data.js

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+  },
+  {
+    title: "Grand Canyon",
+    location: "USA",
+    googleMapsUrl: "https://goo.gl/maps/ZVkZD7Quazp6kYUJ6",
+    startDate: "15 Feb, 2021",
+    endDate: "28 Feb, 2021",
+    description: "The Grand Canyon in Arizona is a natural formation distinguished by layered bands of red rock, revealing millions of years of geological history in cross-section. Vast in scale, the canyon averages 10 miles across and a mile deep along its 277-mile length. Much of the area is a national park, with Colorado River white-water rapids and sweeping vistas.",
+    imageUrl: "https://source.unsplash.com/XuQGqCBpNGk"
+  },
+  {
+    title: "Hagia Sophia",
+    location: "Turkey",
+    googleMapsUrl: "https://goo.gl/maps/1sFDwsd5BdSDDbyCA",
+    startDate: "6 Mar, 2021",
+    endDate: "14 Mar, 2021",
+    description: "Originally built by the eastern Roman emperor Justinian I as the Christian cathedral of Constantinople for the state church of the Roman Empire between 532 and 537, and designed by the Greek geometers Isidore of Miletus and Anthemius of Tralles,it was formally called the Church of the Holy Wisdom and was then the world's largest interior space and among the first to employ a fully pendentive dome.",
+    imageUrl: "https://source.unsplash.com/m9dyJ42RiqE"
+  }
+]
+
+export default data;
